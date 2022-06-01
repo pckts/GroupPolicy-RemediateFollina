64b6233cfdd995bc189cb082cd89cff7b576c31e
Packet <atl@pck.dk>
2022-06-01 12:40:22 +0200

    Add files via upload

---
+++ GroupPolicy-RemediateFollina.bat
@@ -0,0 +1,2 @@
+﻿CD /d %~dp0
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dp0GroupPolicy-RemediateFollina.ps1""' -Verb RunAs}"
