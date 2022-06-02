316807f9770cde83e13115804ac8f0138f12e749
Packet <atl@pck.dk>
2022-06-02 21:18:56 +0200

    Delete IntuneScript-RemediateFollina.ps1

--- IntuneScript-RemediateFollina.ps1
+++
@@ -1,2 +0,0 @@
-New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows" -Name "ScriptedDiagnostics" –Force
-New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\ScriptedDiagnostics" -Name "EnableDiagnostics" -Value ”0” -PropertyType "DWORD"
