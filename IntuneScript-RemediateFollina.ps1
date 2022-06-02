72a1ff3852ecd80064f5b1861f13889b560b1e9d
Packet <atl@pck.dk>
2022-06-02 11:38:30 +0200

    Update IntuneScript-RemediateFollina.ps1

--- IntuneScript-RemediateFollina.ps1
+++ IntuneScript-RemediateFollina.ps1
@@ -1,2 +1,2 @@
-﻿New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows" -Name "ScriptedDiagnostics" –Force
+New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows" -Name "ScriptedDiagnostics" –Force
 New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\ScriptedDiagnostics" -Name "EnableDiagnostics" -Value ”0” -PropertyType "DWORD"
