57f00720098157cebe564e3b8ba8197f2f3aa252
Packet <atl@pck.dk>
2022-06-07 23:00:39 +0200

    Update LocalScript-RemediateFollina.ps1

--- LocalScript-RemediateFollina.ps1
+++ LocalScript-RemediateFollina.ps1
@@ -1,3 +1,5 @@
+Set-ExecutionPolicy Unrestricted -force
+New-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies" -Name "Explorer" –Force
 New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "NoReadingPane" -Value ”1” -PropertyType "DWORD"
 New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows" -Name "ScriptedDiagnostics" –Force
 New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\ScriptedDiagnostics" -Name "EnableDiagnostics" -Value ”0” -PropertyType "DWORD"
